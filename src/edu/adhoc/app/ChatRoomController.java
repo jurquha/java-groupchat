b905d9f9654a37d0241a985b8f012d16723af3ad
Jonah Urquhart <jonah.urquhart@gmail.com>
2019-04-24 16:03:40 -0400

    binded scrollpane vvalue property to vbox height property to make sure scroll pane shows new messages

--- src/edu/adhoc/app/ChatRoomController.java
+++ src/edu/adhoc/app/ChatRoomController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -15,10 +16,12 @@ public class ChatRoomController {
     @FXML private VBox messageBox;
     @FXML private VBox userListVBox;
     @FXML private TextField enterTextField;
+    @FXML private ScrollPane messageBoxScrollPane;
 
     @FXML
     protected void handleOnActionEnterField(ActionEvent event) {
         messageBox.getChildren().add(new Text(enterTextField.getText()));
+        messageBoxScrollPane.vvalueProperty().bind(messageBox.heightProperty());
     }
 
 //    @FXML
